0648e066c77deafca5e735be4f88f5604868ea64
DÊNIO <jose.mqmachado@gmail.com>
2022-08-19 13:35:54 -0300

    first

---
+++ ex011.js
@@ -0,0 +1,17 @@
+var idade = 78
+
+if (idade < 16) {
+    console.log('nao vota')
+} 
+
+else if (idade < 18) {
+ console.log('voto opcional')
+}
+
+else if (idade < 70) {
+    console.log('votao obrigatorio ')
+}
+
+else {
+ console.log ('voto opcional')
+}
